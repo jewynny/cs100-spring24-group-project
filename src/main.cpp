d4073e182627dd0c8cceaa15a9daa5dfb0d69835
mathyu03 <mathewthelatios@gmail.com>
2024-02-23 15:19:15 -0800

    added main interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,26 @@
-// empty main 
+#include "../header/Playlist.hpp"
+#include "../header/User.hpp"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void printMenu()
+{
+    User user;
+    string userName;
+    string faveSong;
+
+    cout << "Welcome to Spotilike Music Recommender!";
+    cout << "To begin, please create a user profile.";
+    cout << "Name: ";
+    getline(cin, userName);
+    cout << "\nFavorite song: ";
+    getline(cin, faveSong);
+    user.setName(userName);
+    user.setFaveSong(faveSong);
+
+    cout << "Hello " << userName    
+
+}
+
