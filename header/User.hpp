5962f125fdd9286fbf7f9e0581c7f266ed524bba
jewynny <jgmh003@gmail.com>
2024-03-01 00:19:18 -0800

    Letting user make more than one playlist

--- header/User.hpp
+++ header/User.hpp
@@ -2,24 +2,41 @@
 #define USER_HPP
 
 #include "../header/Playlist.hpp"
-
-#include <iostream>
 #include <vector>
+#include <iostream>
 // using namespace std;
 
 class User {
+    
     public: 
-    // i made these inline, but we can change these - jan
+    User::User()
+    {
+        name = "";
+        faveSong = "";
+    }
+
+    User::User(std::string userName, std::string userFavoriteSong)
+    {
+        name = userName;
+        faveSong = userFavoriteSong;
+    }
+    /*
+    might need these later idk
     void setName(std::string userName) { name = userName; }
     void setFaveSong(std::string song) { faveSong = song; }
-    void makePlaylist();
-    void viewPlaylist();
+    */
+    void addAPlaylist(std::string playlistName);
+    void viewPlaylistSongs();
+    void viewPlaylists();
+
+    //getters need to implement ...
+
+    User::~User(); // for joseph to do hehe
 
     private:
     std::string name;
     std::string faveSong;
-    std::vector<Playlist> Playlist;
-
+    std::vector<Playlist> playlists;
 };
 
 #endif  //USER_HPP
