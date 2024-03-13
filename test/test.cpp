18e2fa63ebeb55c0c214784e4c8bb101b5fc3ef2
jewynny <jgmh003@gmail.com>
2024-03-12 21:47:31 -0700

    Adding template for everyone to write test

---
+++ test/test.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include "../header/Song.hpp"
+#include "../header/Playlist.hpp"
+#include "../header/Recommendations.hpp"
+#include "../header/User.hpp"
+
+TEST(TESTSUITE, TESTTITLE) {
+    // template
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
